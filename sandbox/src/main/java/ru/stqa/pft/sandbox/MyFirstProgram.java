9fb78bac5cb523a14dcf9a4087e40d90c4efc15a
Sergey Poletaev <info.elekit@yandex.ru>
2020-01-16 13:23:21 +0300

    созданы классы Square и Rectangle

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,30 +3,17 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
   public static void main(String[] args) {
     hello("world");
-    hello("user");
-    hello("Sergey");
-    hello("Sergey");
 
+    Square s = new Square(5);
+    System.out.println("площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    double l = 5;
-    System.out.println("площадь квадрата со стороной " + l + " = " + area(l));
-
-    double a = 4;
-    double b = 6;
-    System.out.println("площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    Rectangle r = new Rectangle(6,4);
+    System.out.println("площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
 
   }
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double area(double l) {
-    return l * l;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
   }
 
 }
