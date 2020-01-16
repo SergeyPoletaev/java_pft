177461be5435424bd1d5a5c6efcd64a66ba7b1c8
Sergey Poletaev <info.elekit@yandex.ru>
2020-01-16 20:58:29 +0300

    подключен тестовый фреймворк testng и создан первый тест

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -17,18 +17,10 @@ public class MyFirstProgram {
 
     Point p1 = new Point(3,8);
     Point p2 = new Point(-4,5);
-
     System.out.println("расстояние между точками p1 и p2 = " + p1.distance(p2));
-
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
-
-  /*
-  public static double distance(double x1, double y1, double x2, double y2) {
-    return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-  }
-  */
 }
